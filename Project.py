79927cb6f3879104f087b6885b23e5f5b556d9ad
Penrose Learning <50510199+penroselearning@users.noreply.github.com>
2020-04-01 14:40:29 +0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -1,24 +1,18 @@
 from tkinter import *
 from tkinter.ttk import *
-from PIL import ImageTk, Image
+#from PIL import ImageTk, Image
 
 screen = Tk()
 screen.configure(background='white')
 cart={}
 
 def Window2():
-    def addtocart(item):
-        item = item[0:4]
+    def addtocart():
+        item = cmbvar.get()
         quan = int(quantity.get())
-        if item in cart.keys():
-            for it,qu in cart.items():
-                if it == item:
-                    qu = qu + quan
-        else:
-            pass
-            #cart[item]=quan
-        print (cart)
-
+        print(item)
+        print(quan)
+ 
 
     popup = Toplevel(screen)
     popup.title("Window 2")
@@ -43,42 +37,40 @@ def Window2():
 
     Label(Starter,text="Starters").pack()
     starters = {'Garlic Bread':15,'Mozzerella Sticks':10,'Caeser Salad':20}
-    for item,price in starters.items():
-        text = StringVar()
-        menu_item = f'{item}:{price}$'
-
-        Label(Starter,text=menu_item).pack()
-        quantity = Entry(Starter,style='TEntry')
-        quantity.pack()
-        Button (Starter,text='add to cart',command=lambda: addtocart(menu_item)).pack()
+    
+    menu_item=StringVar(Starter)
+    menu_item.set([key for key in starters][0])
+    Combo1=Combobox(Starter,height=10,width=25,values=[key for key in starters],textvariable=menu_item) #Height refers to number of values it will list without scroll
+    Combo1.pack()
+        
+    quantity=StringVar(Starter)       
+    SpinBox1=Spinbox(Starter,width=10,from_=1,to=10,increment=1,textvariable=quantity) # Always Stores Values as Text
+    SpinBox1.pack()
+    
+    btn=Button (Starter,text='add to cart',command = addtocart)
+    btn.pack()
 
     ###### Main Course
 
     Label(MainCourse, text="Main Course").pack()
 
     mcourse = {'Pesto Penne': 45, 'Margherita Pizza': 55, 'Mushroom Risotto': 50}
-    for item, price in mcourse.items():
-        text = (f'{item}: {price}$')
-        Label(MainCourse, text=text).pack()
-
+    
     ###### Desserts
 
     Label(Dessert, text="Desserts").pack()
     desserts = {'Red Velvet Cake': 25, 'Brownie with Icecream': 30, 'Chocolate Mousse': 20}
-    for item, price in desserts.items():
-        text = (f'{item}:{price}$')
-        Label(Dessert, text=text).pack()
+    
 
+# myimg = Image.open('project.png')
+# myimg = myimg.resize((100,100))
+# myimg = ImageTk.PhotoImage(myimg)
 
-myimg = Image.open('project.png')
-myimg = myimg.resize((100,100))
-myimg = ImageTk.PhotoImage(myimg)
-
-label = Label(screen, image=myimg)
-label.grid(row=1,column=0)
+# label = Label(screen, image=myimg)
+# label.grid(row=1,column=0)
 
 
 
 Button(screen,text="Click Me",command=Window2).grid(row=10,column=0)
 
-screen.mainloop()
+screen.mainloop()# Write your code here :-)
