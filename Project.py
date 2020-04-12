61558c6bbe8b6c41634a8002ab71b5758622e1b4
Penrose Learning <50510199+penroselearning@users.noreply.github.com>
2020-04-12 12:24:51 +0400

    Update Project.py

--- Project.py
+++ Project.py
@@ -5,23 +5,40 @@ from tkinter.ttk import *
 
 screen = Tk()
 screen.configure(background='white')
-cart = {'Garlic Bread': 0, 'Mozzerella Sticks': 0, 'Caeser Salad': 0,'Pesto Penne': 0, 'Margherita Pizza': 0, 'Mushroom Risotto': 0,'Red Velvet Cake': 0, 'Brownie with Icecream': 0, 'Chocolate Mousse': 0}
 
+cart ={}
 
 def Window2():
-    def addtocart():
-        item = menu_item.get()
-        quan = int(quantity.get())
-        for food,number in cart.items():
-            number = number+quan
-        print (cart)
-    popup = Toplevel(screen)
-    popup.title("Window 2")
-    popup.configure(background='red')
+    
+    def addtocart(menu_item,qty,price):
+         
+        #price = [price for price in menu_item.keys() if me
+        cart[menu_item] = [qty,int(qty)*int(price)]
+        print(cart)
+    
+    def viewcart():
+        displaycart = Toplevel(placeorder)
+        displaycart.geometry("650x300")
+        displaycart.configure(background='lightblue')
+        displaycart.title("View Cart")
+        
+        tv = Treeview(displaycart, show='headings', columns=["Menu Item","Quantity","Price"], height=9)
+     
+        for col in ["Menu Item","Quantity","Price"]:
+             tv.heading(col, text=col, command=lambda _col=col: treeview_sort_column(tv, _col, False))
+        tv.pack()
 
-    Label(popup, text="Menu Selection").pack()
+        for food,details in cart.items():
+            tv.insert("", END, values=(food,details[0],f"AED {details[1]}"))
+    
+    
+    placeorder = Toplevel(screen)
+    placeorder.title("Window 2")
+    placeorder.configure(background='red')
 
-    Tabs = Notebook(popup, width=600)
+    Label(placeorder, text="Menu Selection").pack()
+
+    Tabs = Notebook(placeorder, width=600)
     Tabs.pack()
 
     Starter = Frame(Tabs, borderwidth=10)
@@ -32,43 +49,58 @@ def Window2():
 
     Dessert = Frame(Tabs, borderwidth=10)
     Tabs.add(Dessert, text="Dessert")
-
+    
     ###### Starters
 
     Label(Starter, text="Starters").pack()
-    starters = {'Garlic Bread': 15, 'Mozzerella Sticks': 10, 'Caeser Salad': 20}
+    starters = {'Garlic Bread': 15, 'Mozzerella Sticks': 10, 
+    'Caeser Salad': 20}
 
-    menu_item = StringVar(Starter)
-    menu_item.set([key for key in starters][0])
+    print([price for (item,price) in starters.items() if item=="Garlic Bread"][0])
+    starter = StringVar(Starter)
+    starter.set([key for key in starters][0])
     Combo1 = Combobox(Starter, height=10, width=25, values=[key for key in starters],
-                      textvariable=menu_item)  # Height refers to number of values it will list without scroll
+                      textvariable=starter)  # Height refers to number of values it will list without scroll
     Combo1.pack()
 
-    quantity = StringVar(Starter)
+    starter_qty = StringVar(Starter)
     SpinBox1 = Spinbox(Starter, width=10, from_=1, to=10, increment=1,
-                       textvariable=quantity)  # Always Stores Values as Text
+                       textvariable=starter_qty)  # Always Stores Values as Text
     SpinBox1.pack()
 
-    btn = Button(Starter, text='add to cart', command=addtocart)
+    btn = Button(Starter, text='add to cart', 
+    command=lambda: addtocart(starter.get(),
+    starter_qty.get(),
+    [price for (item,price) in starters.items() if item==starter.get()][0]))
     btn.pack()
+    
+    btn = Button(Starter, text='View Cart', command=viewcart)
+    btn.pack()
+    
 
     ###### Main Course
 
     Label(MainCourse, text="Main Course").pack()
 
     mcourse = {'Pesto Penne': 45, 'Margherita Pizza': 55, 'Mushroom Risotto': 50}
-    menu_item = StringVar(MainCourse)
-    menu_item.set([key for key in mcourse][0])
+    
+    maincourse = StringVar(MainCourse)
+    maincourse.set([key for key in mcourse][0])
     Combo1 = Combobox(MainCourse, height=10, width=25, values=[key for key in mcourse],
-                      textvariable=menu_item)  # Height refers to number of values it will list without scroll
+                      textvariable=maincourse)  # Height refers to number of values it will list without scroll
     Combo1.pack()
 
-    quantity = StringVar(MainCourse)
+    maincourse_qty = StringVar(MainCourse)
     SpinBox1 = Spinbox(MainCourse, width=10, from_=1, to=10, increment=1,
-                       textvariable=quantity)  # Always Stores Values as Text
+                       textvariable=maincourse_qty)  # Always Stores Values as Text
     SpinBox1.pack()
 
-    btn = Button(MainCourse, text='add to cart', command=addtocart)
+    btn = Button(MainCourse, text='add to cart', 
+    command= lambda:addtocart(maincourse.get(),
+    maincourse_qty.get(),
+    [price for (item,price) in mcourse.items() if item==maincourse.get()][0]))
+    btn.pack()
+    btn = Button(MainCourse, text='View Cart', command=viewcart)
     btn.pack()
 
     ###### Desserts
@@ -76,19 +108,25 @@ def Window2():
     Label(Dessert, text="Desserts").pack()
 
     desserts = {'Red Velvet Cake': 25, 'Brownie with Icecream': 30, 'Chocolate Mousse': 20}
-    menu_item = StringVar(Dessert)
-    menu_item.set([key for key in desserts][0])
+    dessert = StringVar(Dessert)
+    dessert.set([key for key in desserts][0])
     Combo1 = Combobox(Dessert, height=10, width=25, values=[key for key in desserts],
-                      textvariable=menu_item)  # Height refers to number of values it will list without scroll
+                      textvariable=dessert)  # Height refers to number of values it will list without scroll
     Combo1.pack()
 
-    quantity = StringVar(Dessert)
+    dessert_qty = StringVar(Dessert)
     SpinBox1 = Spinbox(Dessert, width=10, from_=1, to=10, increment=1,
-                       textvariable=quantity)  # Always Stores Values as Text
+                       textvariable=dessert_qty)  # Always Stores Values as Text
     SpinBox1.pack()
 
-    btn = Button(Dessert, text='add to cart', command=addtocart)
+    btn = Button(Dessert, text='add to cart', 
+    command=lambda:addtocart(dessert.get(),
+    dessert_qty.get(),
+    [price for (item,price) in desserts.items() if item==dessert.get()][0]))
     btn.pack()
+    btn = Button(Dessert, text='View Cart', command=viewcart)
+    btn.pack()
+    
 
 
 # myimg = Image.open('project.png')
@@ -98,7 +136,6 @@ def Window2():
 # label = Label(screen, image=myimg)
 # label.grid(row=1,column=0)
 
-
 Button(screen, text="Click Me", command=Window2).grid(row=10, column=0)
 
-screen.mainloop()  # Write your code here :-)
+screen.mainloop()
